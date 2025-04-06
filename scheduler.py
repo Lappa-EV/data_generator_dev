c5b960b9a0fb1b11945cc4ad2b46e300eda4d565
Lappa-EV <lapp-ka@bk.ru>
2025-04-06 12:38:12 +0700

    sheduler_time.sleep

--- scheduler.py
+++ scheduler.py
@@ -1,11 +1,11 @@
 import time
 from data_generator import job_generate_and_save
 
-# Запускаем задачу сразу и потом через каждые 23 ч. 59 мин. (82800 сек.)
+# Запускаем задачу сразу и потом через каждые 24 ч. (86400 сек.)
 if __name__ == "__main__":
     num_rows = int(input("Введите количество строк для генерации: "))  # Ввод количества строк один раз
     job_generate_and_save(num_rows)  # Выполняем задачу сразу при запуске
 
     while True:
-        time.sleep(82800)  # Ждем 23 часа 59 минут
+        time.sleep(86400)  # Ждем 24 часа
         job_generate_and_save(num_rows)  # Выполняем задачу снова с тем же количеством строк
